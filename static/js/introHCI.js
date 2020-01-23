4a8952029dcb1f192178f2ff619df1b83f3d620a
Nicholas Schenone <nschenone16@gmail.com>
2020-01-23 12:54:50 -0800

    Lab3

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Call this function when the page loads (the "ready" event)
-$(document).ready(function() {
+$(document).ready(function () {
 	initializePage();
 })
 
@@ -9,10 +9,39 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+	$("#testjs").click(function (e) {
+
+		$('.jumbotron h1').text("Web development is fun");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").toggleClass("active");
 	});
+
+	$("a.thumbnail").click(projectClick);
+
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+}
+
+var toggle = 0;
+function projectClick(e) {
+	console.log("Project clicked");
+	e.preventDefault();
+
+	var containingProject = $(this).closest(".project");
+	var description = $(containingProject).find(".project-description");
+
+	if (toggle == 0) {
+		$(this).css("background-color", "green");
+	} else {
+		$(this).css("background-color", "white");
+	}
+
+	if (description.length == 0) {
+		$(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+	} else {
+		description.fadeToggle("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+	}
+
+	toggle ^= 1;
 }
